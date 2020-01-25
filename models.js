94087443fe6586a73c06e17e83a4f46b3f0d7f3e
Matt <matt.hinton@piano.io>
2020-01-25 16:42:16 -0500

    Initial commit

---
+++ models.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+// this is our schema to represent a blog post
+const blogpostSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  content: {type: String, required: true},
+  author: {
+    firstName: String,
+    lastName: String
+  },
+  created: {type: Date, default: Date.now}
+});
+
+// *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
+// allow us to define properties on our object that manipulate
+// properties that are stored in the database. Here we use it
+// to generate a human readable string based on the author object
+// we're storing in Mongo.
+blogpostSchema.virtual('authorString').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim()});
+
+
+// this is an *instance method* which will be available on all instances
+// of the model. This method will be used to return an object that only
+// exposes *some* of the fields we want from the underlying data
+blogpostSchema.methods.apiRepr = function() {
+
+  return {
+    id: this._id,
+    title: this.title,
+    content: this.content,
+    author: this.authorString
+  };
+}
+
+// note that all instance methods and virtual properties on our
+// schema must be defined *before* we make the call to `.model`.
+const Blogpost = mongoose.model('Blogpost', blogpostSchema);
+
+module.exports = {Blogpost};
